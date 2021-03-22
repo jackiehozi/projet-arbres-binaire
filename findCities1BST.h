685799c4be6f07f1bf7157dea8edb7c23faed4cd
Jackie <78017364+jackiehozi@users.noreply.github.com>
2021-03-22 17:48:42 +0100

    Create findCities1BST.h

---
+++ findCities1BST.h
@@ -0,0 +1,23 @@
+#ifndef FIND_CITIES_BST
+#define FIND_CITIES_BST
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include "City.h"
+#include "LinkedList.h"
+
+
+
+
+
+
+LinkedList* findCities(LinkedList* cities, double latitudeMin, double latitudeMax, double longitudeMin, double longitudeMax);
+
+
+#endif
